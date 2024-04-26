dedabc823657f2281e7ec93b39eba38ee052ba0f
abhay2003vyas <142084995+abhay2003vyas@users.noreply.github.com>
2024-04-27 00:48:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 // JavaScript
 document.addEventListener('DOMContentLoaded', function() {
-    var urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('qrdata')) {
-      var qrData = JSON.parse(urlParams.get('qrdata'));
-      document.getElementById('name').value = qrData.name || '';
-      document.getElementById('address').value = qrData.address || '';
-      document.getElementById('mobile').value = qrData.mobile || '';
-      document.getElementById('email').value = qrData.email || '';
+  // Function to parse URL parameters
+  function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  };
+
+  // Fill form fields with data from QR code if available
+  var qrData = getUrlParameter('qrdata');
+  if (qrData) {
+    try {
+      var decodedData = JSON.parse(qrData);
+      document.getElementById('name').value = decodedData.name || '';
+      document.getElementById('address').value = decodedData.address || '';
+      document.getElementById('mobile').value = decodedData.mobile || '';
+      document.getElementById('email').value = decodedData.email || '';
+    } catch (error) {
+      console.error('Error parsing JSON data:', error);
     }
-  });
-  
+  }
+});
